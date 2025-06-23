e8e0e9539797e135be264a49d70b863b87286975
KennethH <kennethchayes@gmail.com>
2025-06-23 13:19:59 -0400

    added some more navigation tabs

--- frontend/app/navigation/KidsTabs.tsx
+++ frontend/app/navigation/KidsTabs.tsx
@@ -10,3 +10,5 @@ export default function KidsTabs() {
     </Tab.Navigator>
   );
 }
+
+export default KidsTabs;
--- frontend/app/navigation/ParentTabs.tsx
+++ frontend/app/navigation/ParentTabs.tsx
@@ -14,3 +14,5 @@ export default function ParentTabs() {
     </Tab.Navigator>
   );
 }
+
+export default ParentTabs;
