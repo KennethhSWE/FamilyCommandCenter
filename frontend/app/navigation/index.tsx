10f76a6745c1beee569db45a180117122a30d32a
KennethH <kennethchayes@gmail.com>
2025-06-24 21:31:14 -0400

    testing

--- frontend/app/navigation/index.tsx
+++ frontend/app/navigation/index.tsx
@@ -1,23 +1,31 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SplashScreen from '../screens/SplashScreen';
-import LoginScreen from '../screens/LoginScreen';
-import RegisterScreen from '../screens/RegisterScreen';
-import ParentTabs from './ParentTabs';
-import KidsTabs from './KidsTabs';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SplashScreen from "../screens/SplashScreen";
+import LoginScreen from "../screens/LoginScreen";
+import RegisterScreen from "../screens/RegisterScreen";
+import ParentTabs from "./ParentTabs";
+import KidsTabs from "./KidsTabs";
 
 const Stack = createNativeStackNavigator();
 
 export default function Navigation() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Splash">
-        <Stack.Screen name="Splash" component={SplashScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Parent" component={ParentTabs} />
-        <Stack.Screen name="Kids" component={KidsTabs} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator
+      screenOptions={{ headerShown: false }}
+      initialRouteName="Splash"
+    >
+      <Stack.Screen name="Splash">
+        {(props) => (
+          <SplashScreen
+            {...props}
+            onFinish={() => props.navigation.replace("Login")}
+          />
+        )}
+      </Stack.Screen>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="Parent" component={ParentTabs} />
+      <Stack.Screen name="Kids" component={KidsTabs} />
+    </Stack.Navigator>
   );
 }
